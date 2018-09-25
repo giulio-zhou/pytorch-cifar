2aed2cbc1af8b1085fe5d9d198700cfbd27b0997
Angela Jiang <to.ajiang@gmail.com>
2018-09-25 15:09:08 -0400

    Add sampling min to script

--- scripts/run_cifar10_sampling.sh
+++ scripts/run_cifar10_sampling.sh
@@ -6,6 +6,7 @@ POOL_SIZE=$3
 LR=$4
 DECAY=$5
 MAX_NUM_BACKPROPS=$6
+SAMPLING_MIN=$7
 
 mkdir "/proj/BigLearning/ahjiang/output/cifar10/"
 OUTPUT_DIR="/proj/BigLearning/ahjiang/output/cifar10/"$EXP_NAME
@@ -13,12 +14,11 @@ PICKLE_DIR=$OUTPUT_DIR/pickles
 mkdir $OUTPUT_DIR
 mkdir $PICKLE_DIR
 
-
 NUM_TRIALS=3
 for i in `seq 1 $NUM_TRIALS`
 do
-  OUTPUT_FILE="sampling_cifar10_"$NET"_0_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i"_v2"
-  PICKLE_PREFIX="sampling_cifar10_"$NET"_0_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i
+  OUTPUT_FILE="sampling_cifar10_"$NET"_"$SAMPLING_MIN"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i"_v2"
+  PICKLE_PREFIX="sampling_cifar10_"$NET"_"$SAMPLING_MIN"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
@@ -31,5 +31,6 @@ do
     --max-num-backprops=$MAX_NUM_BACKPROPS \
     --pickle-dir=$PICKLE_DIR \
     --pickle-prefix=$PICKLE_PREFIX \
+    --sampling-min=$SAMPLING_MIN\
     --lr $LR &> $OUTPUT_DIR/$OUTPUT_FILE
 done
