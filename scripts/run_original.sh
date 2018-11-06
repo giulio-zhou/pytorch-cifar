0df222e8f4fb492d32fed319ad6527fb93e8f6e3
Angela Jiang <to.ajiang@gmail.com>
2018-11-05 19:33:49 -0700

    Update debug experiment name

--- scripts/run_original.sh
+++ scripts/run_original.sh
@@ -1,10 +1,7 @@
-expname="181103_kuangliu"
+expname="181105_debug"
 outfile="/proj/BigLearning/ahjiang/output/cifar10/$expname/kuangliu_cifar10_resnet_1_128_0.0_0.0005_trial1_v2"
-python main.py -e=150 > $outfile
-python main.py --resume --lr=0.01 -e=100 >> $outfile
-python main.py --resume --lr=0.001 -e=100 >> $outfile
-
-outfile="/proj/BigLearning/ahjiang/output/cifar10/$expname/kuangliu_cifar10_resnet_1_128_0.0_0.0005_trial2_v2"
-python main.py -e=150 > $outfile
+python main.py -e=50 > $outfile
+python main.py --resume -e=50 >> $outfile
+python main.py --resume -e=50 >> $outfile
 python main.py --resume --lr=0.01 -e=100 >> $outfile
 python main.py --resume --lr=0.001 -e=100 >> $outfile
