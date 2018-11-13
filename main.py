fa6395d15b3c4842de4f0128799989e81f6f40f9
Angela Jiang <to.ajiang@gmail.com>
2018-11-12 18:50:55 -0700

    Add debugging steps Add augment flag, debug printing

--- main.py
+++ main.py
@@ -18,15 +18,19 @@ import argparse
 from models import *
 from utils import progress_bar
 
+DEBUG = True
 
-seed = 1337
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.backends.cudnn.deterministic = True
+if DEBUG:
+    print("Setting static random seeds")
+    seed = 1337
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--augment', '-a', action='store_true', help='Turn on data augmentation')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--epochs', '-e', default=150, type=int, help='number of epochs')
 args = parser.parse_args()
@@ -39,12 +43,20 @@ global_num_backpropped = 0 # start from epoch 0 or last checkpoint num_backpropp
 
 # Data
 print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+if args.augment:
+    print("Performing data augmentation on CIFAR10")
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+else:
+    print("Not performing data augmentation on CIFAR10")
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
@@ -52,6 +64,7 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainset = [i for i in trainset]
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
@@ -93,6 +106,7 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5
 # Training
 def train(epoch):
     global global_num_backpropped
+    global DEBUG
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
@@ -104,6 +118,11 @@ def train(epoch):
         outputs = net(inputs)
         loss = criterion(outputs, targets)
 
+        if DEBUG:
+            print("[DEBUG train] output:", outputs.data.cpu().numpy()[0])
+            print("[DEBUG train] targets:", targets.data.cpu().numpy()[0])
+            print("[DEBUG train] loss:", loss.item())
+
         loss.backward()
         optimizer.step()
         global_num_backpropped += len(inputs)
@@ -113,28 +132,54 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        if batch_idx % 10 == 0:
-            print('train_debug,{},{},{},{:.6f},{:.6f},{},{:.6f}'.format(
-                epoch,
-                global_num_backpropped,
-                0,
-                train_loss/(batch_idx+1),
-                train_loss/(batch_idx+1),
-                0,
-                100.*correct/total))
+        # DEBUG
+        # if batch_idx % 10 == 0:
+        print('train_debug,{},{},{},{:.6f},{:.6f},{},{:.6f}'.format(
+            epoch,
+            global_num_backpropped,
+            0,
+            train_loss/(batch_idx+1),
+            train_loss/(batch_idx+1),
+            0,
+            100.*correct/total))
+    if DEBUG:
+        s = torch.sum(net.module.conv1.weight.data)
+        print("[DEBUG train] Weight sum:", s.item())
+        s = torch.sum(net.module.bn1.weight.data)
+        print("[DEBUG train] Weight sum:", s.item())
+        s = torch.sum(net.module.linear.weight.data)
+        print("[DEBUG train] Weight sum:", s.item())
 
 def test(epoch):
     global best_acc
     global global_num_backpropped
+    global DEBUG
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
+
+    if DEBUG:
+        s = torch.sum(net.module.conv1.weight.data)
+        print("[DEBUG test] Weight sum:", s.item())
+        s = torch.sum(net.module.bn1.weight.data)
+        print("[DEBUG test] Weight sum:", s.item())
+        s = torch.sum(net.module.linear.weight.data)
+        print("[DEBUG test] Weight sum:", s.item())
+
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
+
             loss = criterion(outputs, targets)
+
+            print("[DEBUG test] net.eval?:", net.training)
+            if DEBUG:
+                print("[DEBUG test] output:", outputs.data.cpu().numpy()[-1])
+                print("[DEBUG test] BS:", len(inputs.data.cpu().numpy()))
+                print("[DEBUG test] target:", targets.data.cpu().numpy()[-1])
+                print("[DEBUG test] loss:", loss.item())
 
             test_loss += loss.item()
             _, predicted = outputs.max(1)
