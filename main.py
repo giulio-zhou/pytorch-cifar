2d0ac9a79c274802575aea3606c96873ce74455c
Angela Jiang <to.ajiang@gmail.com>
2018-11-15 08:06:11 -0700

    Remove unnecessary trainset iteration

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainset = [i for i in trainset]
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
