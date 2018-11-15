f5aac321f3a791de916156eb1bfb0e9fa57edfbc
Angela Jiang <to.ajiang@gmail.com>
2018-11-15 09:38:34 -0700

    Add hacky reproduce_orig.sh for 181115_seedednew_aug

--- scripts/run_cifar10_reproduce_orig.sh
+++ scripts/run_cifar10_reproduce_orig.sh
@@ -1,4 +1,5 @@
 expname=$1
+SAMPLING_MIN=$2
 
 set -x
 
@@ -12,7 +13,6 @@ BATCH_SIZE=128
 LR="data/config/lr_sched_orig"
 DECAY=0.0005
 MAX_NUM_BACKPROPS=17500000
-SAMPLING_MIN=$2
 
 EXP_NAME=$EXP_PREFIX
 
@@ -26,8 +26,8 @@ NUM_TRIALS=5
 for i in `seq 1 $NUM_TRIALS`
 do
 
-  OUTPUT_FILE="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_v2"
-  PICKLE_PREFIX="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i
+  OUTPUT_FILE="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed1337_v2"
+  PICKLE_PREFIX="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed1337"
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
@@ -40,6 +40,6 @@ do
     --pickle-dir=$PICKLE_DIR \
     --pickle-prefix=$PICKLE_PREFIX \
     --sampling-min=$SAMPLING_MIN \
+    --augment \
     --lr-sched $LR &> $OUTPUT_DIR/$OUTPUT_FILE
-    #--augment \
 done
