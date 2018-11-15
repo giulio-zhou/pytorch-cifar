632a296932f98f2b3345adb23c9e4cf1d75039a7
Angela Jiang <to.ajiang@gmail.com>
2018-11-14 19:59:50 -0700

    Add trial to filename properly

--- scripts/run_original.sh
+++ scripts/run_original.sh
@@ -6,7 +6,7 @@ mkdir "/proj/BigLearning/ahjiang/output/cifar10/$expname/"
 NUM_TRIALS=5
 for i in `seq 1 $NUM_TRIALS`
 do
-  outfile="/proj/BigLearning/ahjiang/output/cifar10/$expname/kuangliu_cifar10_resnet_1_128_0.0_0.0005_trial$i_v2"
+  outfile="/proj/BigLearning/ahjiang/output/cifar10/$expname/kuangliu_cifar10_resnet_1_128_0.0_0.0005_trial"$i"_v2"
   python main.py -e=50 --augment --checkpoint=$expname > $outfile
   python main.py --resume -e=50 --augment --checkpoint=$expname >> $outfile
   python main.py --resume -e=50 --augment --checkpoint=$expname >> $outfile
