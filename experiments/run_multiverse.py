3d043ef7a3252aecb952e0d78849a6f89f690d09
Angela Jiang <to.ajiang@gmail.com>
2018-12-12 20:34:12 -0700

    Add topk experiment to multiverse

--- experiments/run_multiverse.py
+++ experiments/run_multiverse.py
@@ -34,7 +34,7 @@ def make_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def run_experiment(args, experiment_name):
+def run_experiment(args, experiment_name, seed, trial):
 
     # Directory management
     base_directory = "/proj/BigLearning/ahjiang/output/"
@@ -45,22 +45,17 @@ def run_experiment(args, experiment_name):
     make_dir(output_directory)
     make_dir(pickle_directory)
 
-    # Random seed management
-    num_seeds = 1
-    seeds = [i + 1337 for i in range(0, num_seeds * 10, 10)]
+    output_file = get_output_file(args, trial+1, seed)
+    args.pickle_prefix = get_output_prefix(args, trial+1, seed)
 
-    for trial, seed in enumerate(seeds):
-        output_file = get_output_file(args, trial+1, seed)
-        args.pickle_prefix = get_output_prefix(args, trial+1, seed)
-
-        # Capture stdout to output file and run experiment
-        stdout_ = sys.stdout
-        output_path = os.path.join(output_directory, output_file)
-        print("Writing results to {}".format(output_path))
-        sys.stdout = open(output_path, 'w')
-        main.main(args)
-        sys.stdout.flush()
-        sys.stdout = stdout_
+    # Capture stdout to output file and run experiment
+    stdout_ = sys.stdout
+    output_path = os.path.join(output_directory, output_file)
+    print("Writing results to {}".format(output_path))
+    sys.stdout = open(output_path, 'w')
+    main.main(args)
+    sys.stdout.flush()
+    sys.stdout = stdout_
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch SB Training')
@@ -72,12 +67,11 @@ if __name__ == "__main__":
     parser.add_argument('--experiment-prefix', help='experiment prefix')
     parser.add_argument('--checkpoint-prefix', help='checkpoint file name without epoch')
     parser.add_argument('--num-extra-epochs', type=int, help='number of epochs to perform experiment for')
+    parser.add_argument('--num-trials', type=int, help='number of trials')
+    parser.add_argument('--topk', action='store_true', help='Use TopK selector')
     args = parser.parse_args()
 
     checkpoint_epochs = [int(e) for e in args.checkpoint_epochs.split(",")]
-
-    #args.sb_strategy = "sampling"
-    #print("Overriding sb strategy to {}".format(args.sb_strategy))
 
     args.max_num_backprops = 50000 * args.num_extra_epochs
     print("Overriding max num backprops to {}".format(args.max_num_backprops))
@@ -93,18 +87,25 @@ if __name__ == "__main__":
     else:
         selectivities = [0.2, 1, 0.4, 0.6, 0.8]
 
-    for epoch in checkpoint_epochs:
-        checkpoint_file = "{}_epoch{}_ckpt.t7".format(args.checkpoint_prefix,
-                                                      epoch)
-        args.resume_checkpoint_file = checkpoint_file
-        print("Overriding resume_checkpoint_file to {}".format(args.resume_checkpoint_file))
-        experiment_name = "{}_epoch{}".format(args.experiment_prefix, 
-                                              epoch)
+    # Random seed management
+    for trial in range(args.num_trials):
+        seed = trial + 1337
+        for epoch in checkpoint_epochs:
+            checkpoint_file = "{}_epoch{}_ckpt.t7".format(args.checkpoint_prefix,
+                                                          epoch)
+            args.resume_checkpoint_file = checkpoint_file
+            print("Overriding resume_checkpoint_file to {}".format(args.resume_checkpoint_file))
+            experiment_name = "{}_epoch{}".format(args.experiment_prefix, 
+                                                  epoch)
 
-        for selectivity in selectivities:
-            args.sampling_min = selectivity
-            print("Overriding sampling_min to {}".format(args.sampling_min))
-            #args.sampling_max = selectivity
-            #print("Overriding sampling_max to {}".format(args.sampling_max))
-            run_experiment(args, experiment_name)
+            for selectivity in selectivities:
+                if args.topk:
+                    args.sb_strategy = "topk"
+                    print("Overriding sb_strategy to {}".format(args.sb_strategy))
+                    args.sample_size = int(args.batch_size * selectivity)
+                    print("Overriding sample_size to {}".format(args.sample_size))
+                else:
+                    args.sampling_min = selectivity
+                    print("Overriding sampling_min to {}".format(args.sampling_min))
+                run_experiment(args, experiment_name, seed, trial)
 
