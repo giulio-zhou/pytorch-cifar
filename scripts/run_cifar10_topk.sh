c844f86501a94a2818299f709315b7df5e4c58ea
Angela Jiang <to.ajiang@gmail.com>
2018-12-12 20:34:54 -0700

    Add run_cifar10_topk

--- scripts/run_cifar10_topk.sh
+++ scripts/run_cifar10_topk.sh
@@ -1,12 +1,23 @@
+expname=$1
+SAMPLE_SIZE=$2
+
 set -x
 
-EXP_NAME=$1
-NET=$2
-TOP_K=$3
-POOL_SIZE=$4
-LR=$5
-DECAY=$6
-MAX_NUM_BACKPROPS=$7
+ulimit -n 2048
+ulimit -a
+
+EXP_PREFIX=$expname
+SAMPLING_MIN=0
+SAMPLING_STRATEGY="topk"
+NUM_TRIALS=1
+NET="resnet"
+BATCH_SIZE=128
+LR="data/config/lr_sched_orig"
+DECAY=0.0005
+MAX_NUM_BACKPROPS=17500000
+SEED=1337
+
+EXP_NAME=$EXP_PREFIX
 
 mkdir "/proj/BigLearning/ahjiang/output/cifar10/"
 OUTPUT_DIR="/proj/BigLearning/ahjiang/output/cifar10/"$EXP_NAME
@@ -14,23 +25,26 @@ PICKLE_DIR=$OUTPUT_DIR/pickles
 mkdir $OUTPUT_DIR
 mkdir $PICKLE_DIR
 
-
-NUM_TRIALS=3
 for i in `seq 1 $NUM_TRIALS`
 do
-  OUTPUT_FILE="topk_cifar10_"$NET"_"$TOP_K"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i"_v2"
-  PICKLE_PREFIX="topk_cifar10_"$NET"_"$TOP_K"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i
+
+  OUTPUT_FILE=$SAMPLING_STRATEGY"_cifar10_"$NET"_"$SAMPLE_SIZE"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED"_v2"
+  PICKLE_PREFIX=$SAMPLING_STRATEGY"_cifar10_"$NET"_"$SAMPLE_SIZE"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
   python main.py \
-    --batch-size=1 \
-    --top-k=$TOP_K \
+    --sb-strategy=$SAMPLING_STRATEGY \
+    --sample-size=$SAMPLE_SIZE \
     --net=$NET \
-    --pool-size=$POOL_SIZE \
+    --batch-size=$BATCH_SIZE \
     --decay=$DECAY \
     --max-num-backprops=$MAX_NUM_BACKPROPS \
     --pickle-dir=$PICKLE_DIR \
     --pickle-prefix=$PICKLE_PREFIX \
-    --lr $LR &> $OUTPUT_DIR/$OUTPUT_FILE
+    --sampling-min=$SAMPLING_MIN \
+    --sb-start-epoch=1 \
+    --augment \
+    --seed=$SEED \
+    --lr-sched $LR &> $OUTPUT_DIR/$OUTPUT_FILE
 done
