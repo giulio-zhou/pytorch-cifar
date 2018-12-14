d9bc28fbea636353b8279c31608a3e8ede42a872
Angela Jiang <to.ajiang@gmail.com>
2018-12-14 08:01:02 -0700

    No prime script uses sampling

--- scripts/noparams/run_cifar10_noprime.sh
+++ scripts/noparams/run_cifar10_noprime.sh
@@ -11,7 +11,7 @@ ulimit -n 2048
 ulimit -a
 
 EXP_PREFIX=$expname
-SAMPLING_STRATEGY="deterministic"
+SAMPLING_STRATEGY="sampling"
 LR="data/config/lr_sched_orig"
 DECAY=0.0005
 MAX_NUM_BACKPROPS=17500000
@@ -28,8 +28,8 @@ mkdir $PICKLE_DIR
 for i in `seq 1 $NUM_TRIALS`
 do
 
-  OUTPUT_FILE="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED"_v2"
-  PICKLE_PREFIX="deterministic_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED
+  OUTPUT_FILE="sampling_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED"_v2"
+  PICKLE_PREFIX="sampling_cifar10_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
